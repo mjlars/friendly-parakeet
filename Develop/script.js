dd017d2b10c1d3831a1322531b6e514e487c88cf
mjlars <maflarson@gmail.com>
2021-10-02 14:06:03 -0600

    got it working for 11 digit passwords

--- Develop/script.js
+++ Develop/script.js
@@ -6,11 +6,21 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    function generatePassword() {
+      var length = 11,
+          charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[^_`{|}~",
+          retVal = "";
+      for (var i = 0, n = charset.length; i < length; ++i) {
+          retVal += charset.charAt(Math.floor(Math.random() * n));
+      }
+      return retVal;
+    } 
 
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
